2b054d4ae2b92a306d28025b788f5098d050e824
Sheetal <sheetalpangarkar@gmail.com>
2021-10-24 16:58:44 +0530

    inheritancecode

---
+++ src/package1/ParentClass.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class ParentClass {
+	public void Engine()
+	{
+		System.out.println("i am parent engine");
+	}
+public void breaks ()
+{
+	System.out.println("i am parents breaks");
+	}
+public void bodyColor () 
+{
+	System.out.println("i am parents color");
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
